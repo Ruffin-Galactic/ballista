098620eb36c6a5387886afc59b87730dcd9daf62
prasanth-galactic <prasanth@ruffingalactic.com>
2025-06-29 22:03:03 +0530

    Update client.py

--- pyballista/client.py
+++ pyballista/client.py
@@ -1,14 +1,14 @@
 import grpc
-import pyballista.proto.scheduler_pb2_grpc as scheduler_pb2_grpc
-import pyballista.proto.scheduler_pb2 as scheduler_pb2
+import pyballista.proto.ballista_pb2_grpc as ballista_pb2_grpc
+import pyballista.proto.ballista_pb2 as ballista_pb2
 
 class BallistaClient:
     def __init__(self, host="localhost", port=50050):
         self.channel = grpc.insecure_channel(f"{host}:{port}")
-        self.client = scheduler_pb2_grpc.BallistaSchedulerStub(self.channel)
+        self.client = ballista_pb2_grpc.BallistaSchedulerStub(self.channel)
 
     def execute_query(self, sql):
-        request = scheduler_pb2.ExecuteQueryParams(
+        request = ballista_pb2.ExecuteQueryParams(
             query=sql,
             settings={"batch_size": "1024"}
         )
@@ -16,5 +16,5 @@ class BallistaClient:
         return response.job_id
 
     def fetch_stages(self, job_id):
-        request = scheduler_pb2.FetchQueryStagesParams(job_id=job_id)
+        request = ballista_pb2.FetchQueryStagesParams(job_id=job_id)
         return self.client.FetchQueryStages(request)
